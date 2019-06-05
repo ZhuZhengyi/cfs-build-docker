9dfd33c5e6c6ecb81fdd6bfdaf548fe81863062a
zhuzhengyi1 <zhuzhengyi@jd.com>
2019-06-05 19:14:30 +0800

    update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,40 @@
 #!/usr/bin/env bash
 
 RootPath=$(cd $(dirname $0) ; pwd)
-CfsBuild="20181126103126/cfs-build:latest"
+CfsClient="chubaofs/centos-ltp:1.0"
+CfsServer="chubaofs/cfs-build:1.0"
 
-docker build -t ${CfsBuild} ${RootPath}
+build_client() {
+    docker build -t ${CfsClient} -f ${RootPath}/Dockerfile-client ${RootPath}
+}
+
+build_server() {
+    docker build -t ${CfsServer} -f ${RootPath}/Dockerfile-server ${RootPath}
+}
+
+push_server() {
+    docker push ${CfsServer}
+}
+
+push_client() {
+    docker push ${CfsClient}
+}
+
+cmd=${1:-"all"}
+
+help() {
+    cat <<EOF
+./build.sh [build | build_server | build_client | push | push_client | push_server]
+EOF
+}
+
+case "$cmd" in
+    "build_client") build_client ;;
+    "build_server") build_server ;;
+    "push_server") push_server ;;
+    "push_client") push_client ;;
+    "build") build_server build_client ;;
+    "push") push_server; push_client ;;
+    *) help ;;
+esac
 
